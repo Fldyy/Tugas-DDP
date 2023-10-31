3724c20f9c19e1356b01a2d07f3257814e6f08dc
Fldyy <145969760+Fldyy@users.noreply.github.com>
2023-10-31 15:55:55 +0700

    soal3.py

--- Tugas Pertemuan 6/soal3.py
+++ Tugas Pertemuan 6/soal3.py
@@ -1,6 +1,6 @@
 jumlah_baris = int(input("Masukkan jumlah baris:"))
 
-for i in range(1, jumlah_bar + 1):
+for i in range(1, jumlah_baris + 1):
     for j in range(i):
         print("*", end=" ")
     print() 
